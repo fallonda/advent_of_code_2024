c3fd295edf1cc7a66be5659888afa588dca9fd39
fallonda <david.j.fallon@gmail.com>
2024-12-18 10:26:21 +0000

    day 2 part 1 complete.

--- 2024-12-02/part_1.py
+++ 2024-12-02/part_1.py
@@ -3,9 +3,39 @@ import numpy as np
 
 input = open("./2024-12-02/input.txt", "r")
 input_list = input.read()
+input.close()
 input_list_split = input_list.split("\n")
 
+# Part 1
+num_safe = 0
+allowed_diffs = set([-3, -2, -1, 1, 2, 3])
 for i in input_list_split:
     as_array = np.array(i.split(" ")).astype(int)
-    for pos, value in enumerate(as_array):
-        print(pos, value)
+    diff = list()
+    for pos, value in enumerate(as_array[0:-1]):
+        diff.append(int(as_array[pos + 1] - as_array[pos]))
+    diff_as_set = set(diff)
+    if len(diff_as_set.difference(allowed_diffs)) == 0:
+        signs = set(np.sign(list(diff_as_set)))
+        if len(signs) == 1:
+            print(as_array)
+            num_safe += 1
+
+# Part 1 answer
+print(num_safe)
+
+# Part 2
+
+num_safe = 0
+allowed_diffs = set([-3, -2, -1, 1, 2, 3])
+for i in input_list_split:
+    as_array = np.array(i.split(" ")).astype(int)
+    diff = list()
+    for pos, value in enumerate(as_array[0:-1]):
+        diff.append(int(as_array[pos + 1] - as_array[pos]))
+    diff_as_set = set(diff)
+    if len(diff_as_set.difference(allowed_diffs)) == 0:
+        signs = set(np.sign(list(diff_as_set)))
+        if len(signs) == 1:
+            print(as_array)
+            num_safe += 1
