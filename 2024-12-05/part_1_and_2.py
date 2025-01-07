59311c266bc5d3038485a63f07f88abe96d8681e
David Fallon <fallon.david@nuritas.com>
2025-01-07 18:06:30 +0000

    Started day 5 part 2.

--- 2024-12-05/part_1_and_2.py
+++ 2024-12-05/part_1_and_2.py
@@ -1,3 +1,5 @@
+import random
+
 # Try the example input in the question
 with open("./2024-12-05/test_input.txt", "r") as f:
     example_input_txt = f.read()
@@ -41,8 +43,9 @@ example_bottom = parse_bottom(example_split_top_and_bottom[1])
 
 def create_orders(order_rules: list, pages: list) -> list:
     middle_num_of_correct_orders = list()
+    incorrect_orders = list()
     for i in pages:
-        print(f"page_instructions: {i}")
+        # print(f"page_instructions: {i}")
         page_orderings = list()
         list_to_pop = i.copy()
         while len(list_to_pop) > 0:
@@ -50,18 +53,25 @@ def create_orders(order_rules: list, pages: list) -> list:
             for k in list_to_pop:
                 to_append = tuple([popped_val, k])
                 page_orderings.append(to_append)
-        print(f"page_orderings: {page_orderings}")
+        # print(f"page_orderings: {page_orderings}")
         # Check that the page orders are allowed
-        check_val = all([(x in order_rules) for x in page_orderings])
-        print(f"allowed: {check_val}")
+        vec_bool = [(x in order_rules) for x in page_orderings]
+        print(f"vec_bool: {vec_bool}")
+        check_val = all(vec_bool)
+        # print(f"allowed: {check_val}")
         if check_val:
             middle_val = i[len(i) // 2]
             middle_num_of_correct_orders.append(middle_val)
-    ret = sum(middle_num_of_correct_orders)
+        else:
+            incorrect_orders.append(i)
+    ret = [sum(middle_num_of_correct_orders), incorrect_orders]
     return ret
 
 
-example_num = create_orders(order_rules=allowed_orders, pages=example_bottom)
+example_sorting = create_orders(
+    order_rules=example_allowed_orders, pages=example_bottom
+)
+example_num = example_sorting[0]
 print(example_num)
 
 # Try part 1
@@ -71,5 +81,9 @@ pt1_split_top_and_bottom = input_txt.split("\n\n")
 
 pt1_allowed_orders = get_codes(pt1_split_top_and_bottom[0])
 pt1_bottom = parse_bottom(pt1_split_top_and_bottom[1])
-pt1_num = create_orders(order_rules=pt1_allowed_orders, pages=pt1_bottom)
+pt1_sorting = create_orders(order_rules=pt1_allowed_orders, pages=pt1_bottom)
+pt1_num = pt1_sorting[0]
 print(pt1_num)
+
+# Part 2
+# Try on the example input
