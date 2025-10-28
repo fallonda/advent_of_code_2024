91a75a4fcfc737940ce299dd087c4c1297a14738
David Fallon <fallon.david@nuritas.com>
2025-10-28 20:31:17 +0000

    Completed day 5 part 2.

--- 2024-12-05/part_1_and_2.py
+++ 2024-12-05/part_1_and_2.py
@@ -41,9 +41,20 @@ def parse_bottom(input: str) -> list:
 example_bottom = parse_bottom(example_split_top_and_bottom[1])
 
 
-def create_orders(order_rules: list, pages: list) -> list:
+def create_orders(order_rules: list, pages: list) -> dict:
+    """Create a dict with the first key:value being the answer for pt1.
+    The second key:value contains a list of lists, being the incorrect orders
+
+    Args:
+        order_rules (list): From get_codes()
+        pages (list): From parse_bottom()
+
+    Returns:
+        dict: {pt1_answer: int, incorrect_orders: list of lists}
+    """
     middle_num_of_correct_orders = list()
     incorrect_orders = list()
+    incorrect_bool_vec = list()
     for i in pages:
         # print(f"page_instructions: {i}")
         page_orderings = list()
@@ -56,7 +67,7 @@ def create_orders(order_rules: list, pages: list) -> list:
         # print(f"page_orderings: {page_orderings}")
         # Check that the page orders are allowed
         vec_bool = [(x in order_rules) for x in page_orderings]
-        print(f"vec_bool: {vec_bool}")
+        # print(f"vec_bool: {vec_bool}")
         check_val = all(vec_bool)
         # print(f"allowed: {check_val}")
         if check_val:
@@ -64,7 +75,12 @@ def create_orders(order_rules: list, pages: list) -> list:
             middle_num_of_correct_orders.append(middle_val)
         else:
             incorrect_orders.append(i)
-    ret = [sum(middle_num_of_correct_orders), incorrect_orders]
+            incorrect_bool_vec.append(vec_bool)
+    ret = {
+        "pt1_answer": sum(middle_num_of_correct_orders),
+        "incorrect_orders": incorrect_orders,
+        "incorrect_bool_vec": incorrect_bool_vec,
+    }
     return ret
 
 
@@ -82,8 +98,39 @@ pt1_split_top_and_bottom = input_txt.split("\n\n")
 pt1_allowed_orders = get_codes(pt1_split_top_and_bottom[0])
 pt1_bottom = parse_bottom(pt1_split_top_and_bottom[1])
 pt1_sorting = create_orders(order_rules=pt1_allowed_orders, pages=pt1_bottom)
-pt1_num = pt1_sorting[0]
+pt1_num = pt1_sorting["pt1_answer"]
 print(pt1_num)
 
+# ----------
 # Part 2
-# Try on the example input
+# ----------
+
+incorrect_orders = pt1_sorting["incorrect_orders"]
+
+
+def get_pages_that_cannot_be_after(order_rules: list, single_order: list, page: int):
+    pages_not_allowed = [
+        x[0] for x in order_rules if (x[1] == page) & (x[0] in single_order)
+    ]
+    return pages_not_allowed
+
+
+def reorder_pages(order_rules: list, single_order: list):
+    mut_list = single_order.copy()
+    for i, page in enumerate(single_order):
+        pages_after = single_order[i + 1 :]
+        pages_that_cannot_be_after = get_pages_that_cannot_be_after(
+            order_rules=order_rules, single_order=single_order, page=page
+        )
+        num_of_pages_that_cannot_be_after = len(pages_that_cannot_be_after)
+        # print({"page": page, "new_pos": num_of_pages_that_cannot_be_after})
+        mut_list[num_of_pages_that_cannot_be_after] = page
+    return mut_list
+
+
+corrected_lists = [
+    reorder_pages(order_rules=pt1_allowed_orders, single_order=x)
+    for x in incorrect_orders
+]
+pt2_answer = create_orders(pt1_allowed_orders, corrected_lists)
+print(pt2_answer)
